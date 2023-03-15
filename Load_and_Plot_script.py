dc63c7af2c9f24fda8ccec162c7a40fbc4348207
Gustav Rørhauge <gustav.rorhauge@hotmail.com>
2023-03-15 14:13:50 +0100

    Update Load_and_Plot_script.py

--- Load_and_Plot_script.py
+++ Load_and_Plot_script.py
@@ -1,9 +1,7 @@
 import numpy as np
-from PIL import Image
 import matplotlib.pyplot as plt
-import random
-import cv2
-
+import seaborn as sns
+#%%
 # Code by Gustav Rørhauge & Andreas Bagge
 
 import os
@@ -25,7 +23,7 @@ n = 20000
 terminate = False
 
 data = np.zeros((n, 13872))
-
+#%%
 for folder in topfolder:
     dirs = os.listdir(path + folder )
     for filename in dirs:
@@ -76,3 +74,33 @@ for i in range(9):
     ax = axs[x,y]
     showim(data[numberofimages[i]][:], brighten = True, ax = ax)
     
+    
+    
+#%%
+meanpic = np.mean(data.reshape(-1,68,68,3),axis=0)    
+#%%
+showim(meanpic, brighten = True)
+# %%
+
+"""covariance, be careful! """
+# cova = np.cov(data.T)
+# #%%
+# sns.heatmap(cova)
+# #%%
+# plt.imshow(cova,cmap = 'viridis')
+
+#%% 
+"""Summary statistics """ 
+channel_len = int(data.shape[1]/3)
+
+dadler = data.reshape(-1,3)
+RGBstd = np.std(dadler,axis=0)
+
+### Standard Deviations
+# stdR = 2388.3 , stdG = 3638.45, stdB = 1912.5
+
+#%%
+lst = np.arange(2700)
+print(f'arange: {lst}')
+picex= lst.reshape(-1,30,30,3)
+print(f'reshaped: {picex}')
