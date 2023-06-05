9406a96d65d0f33593fdc65b81d62837dc3f8b14
Gustav Rørhauge <gustav.rorhauge@hotmail.com>
2023-06-05 10:09:34 +0200

    changed scripts for training

--- training multiple models.py
+++ training multiple models.py
@@ -14,32 +14,15 @@ small_latent_size = 100
 path = "/zhome/5a/2/167858/Desktop/merged_files/" # path to data
 
 rgb_data = Cells(path=path, device=device)
-r_data = Cells(path=path, channel=0, device=device)
-g_data = Cells(path=path, channel=1, device=device)
-b_data = Cells(path=path, channel=2, device=device)
+
 
 args = dict(batch_size=128, drop_last=True, shuffle=True)
-rgb_dataloader = dataloader = DataLoader(rgb_data, **args)
-r_dataloader = dataloader = DataLoader(r_data, **args)
-g_dataloader = dataloader = DataLoader(g_data, **args)
-b_dataloader = dataloader = DataLoader(b_data, **args)
+rgb_dataloader = DataLoader(rgb_data, **args)
 
-rgb_model = VAE(big_latent_size)
-r_model = VAE(small_latent_size, num_channels=1).to(device)
-g_model = VAE(small_latent_size, num_channels=1).to(device)
-b_model = VAE(small_latent_size, num_channels=1).to(device)
+rgb_model = VAE(big_latent_size).to(device)
 
 rgb_stats = rgb_model.train(45, rgb_dataloader, kl_beta = 3, verbose=1)
-r_stats = r_model.train(45, r_dataloader, kl_beta = 3, verbose=1)
-g_stats = g_model.train(45, g_dataloader, kl_beta = 3, verbose=1)
-b_stats = b_model.train(45, b_dataloader, kl_beta = 3, verbose=1)
 
 rgb_model.save_model("rgb_model")
-r_model.save_model("r_model")
-g_model.save_model("g_model")
-b_model.save_model("b_model")
 
 np.save("rgb_model_stats", rgb_stats)
-np.save("r_model_stats", r_stats)
-np.save("g_model_stats", g_stats)
-np.save("b_model_stats", b_stats)
