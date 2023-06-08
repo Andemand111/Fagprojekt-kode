b6cb7bbe3922545b4c025da87f9f8b849011d610
Gustav Rørhauge <gustav.rorhauge@hotmail.com>
2023-06-08 12:15:02 +0200

    small changes

--- kfold.py
+++ kfold.py
@@ -1,17 +1,23 @@
+#%%
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, WeightedRandomSampler, Subset
 from sklearn.model_selection import StratifiedKFold
-from dataset import SmallCells
-from models import ClassifyNN
+from dataset import ClassifyCells, SmallCells
+from models import ClassifyNN, VAE
 import numpy as np
 
-# %%
-data = np.load("celle_data.npy")
-ps = [0.05, 0.1, 0.2, 0.05, 0.1, 0.05, 0.05, 0.1, 0.05, 0.1, 0.1, 0.05]
-labels = np.random.choice(np.arange(12), size=20000, p=ps)
-dataset = SmallCells(data)
+#%%
 
-# %%
+vae = VAE(700)
+vae.load_model("C:/Users/gusta/OneDrive/Skrivebord/KI & Data/Semester 4/Fagprojekt/FærdigeModeller/betamodels/rgb_model_beta")
+#%%
+path = "C:/Users/gusta/OneDrive/Skrivebord/KI & Data/Semester 4/Fagprojekt/Data/singlecell/singh_cp_pipeline_singlecell_images/merged_files/"
+indxs = np.load("C:/Users/gusta/OneDrive/Skrivebord/KI & Data/Semester 4/Fagprojekt/Fagprojekt-kode/moa_indices.npy")
+labels = np.load("C:/Users/gusta/OneDrive/Skrivebord/KI & Data/Semester 4/Fagprojekt/Fagprojekt-kode/moa_int_label.npy")
+labels -= 1
+dataset = ClassifyCells(path, [vae], color_channels = [None], indxs=indxs, labels=labels)
+
+#%%
 
 k = 5
 skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=69)
@@ -32,15 +38,15 @@ for i, (train_index, test_index) in enumerate(skf.split(dataset, labels)):
     label_weights = 1 / counts
     weights = np.array([label_weights[int(i)] for i in test_labels])
     test_sampler = WeightedRandomSampler(
-        weights, num_samples=len(test_labels), replacement=True)
+        weights, num_samples=2000, replacement=True)
 
     train_dataloader = DataLoader(
         train_subset, batch_size=128, sampler=train_sampler, drop_last=True)
     test_dataloader = DataLoader(
-        test_subset, batch_size=len(test_subset), sampler=test_sampler)
+        test_subset, batch_size=128, sampler=test_sampler, drop_last=True)
 
-    model = ClassifyNN(512)
-    model.train(50, train_dataloader)
+    model = ClassifyNN(700)
+    model.train(10, train_dataloader)
     loss = model.test(test_dataloader)
     losses[i] = loss
 
